ed5ae8772c0bd4eeb761659c4449d3d9b6000f68
Randy Houlahan <randy.houlahan@cbd.int>
2021-06-09 16:00:42 -0400

    save

--- src/util/json-api.mjs
+++ src/util/json-api.mjs
@@ -1,6 +1,6 @@
 import   SA                    from 'superagent'
 import { spawnSync, execSync } from 'child_process'
-import   config                from '../../../util/config.mjs'
+import   config                from './config.mjs'
 import consola from 'consola'
 const $http  = SA.agent()
 const global = { enabled: false }
