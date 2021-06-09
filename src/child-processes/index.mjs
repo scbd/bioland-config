dd3589965db793c901b31af4b65d19d6607ebdd0
Randy Houlahan <randy.houlahan@cbd.int>
2021-06-09 09:28:46 -0400

    save

--- src/child-processes/index.mjs
+++ src/child-processes/index.mjs
@@ -4,6 +4,8 @@ runTaskAsChildProcess()
 
 async function runTaskAsChildProcess(){
   const { commandParamCase, branch, commandArgs } = getAllUserArgs()
+
+  console.error('---------------',{ commandParamCase, branch, commandArgs })
   const   taskFunction                            = (await import(`../tasks/${commandParamCase}.mjs`)).default
 
   taskFunction(branch, commandArgs)
