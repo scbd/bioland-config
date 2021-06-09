d0d93beb528ac34818073ff379ebbc213f24a211
Randy Houlahan <randy.houlahan@cbd.int>
2021-06-09 07:21:15 -0400

    save

--- src/util/commands.mjs
+++ src/util/commands.mjs
@@ -22,7 +22,9 @@ export function getCommand({ paramCase } = { paramCase:false }){
   const theCommandTwo       = process.argv[startIndex+1]? changeCase.camelCase(process.argv[ startIndex+1 ]) : ''
 
   consola.warn('getCommand:theCommandOne', theCommandOne)
+  consola.warn('getCommand:isValidCommand(theCommandOne)', isValidCommand(theCommandOne))
   consola.warn('getCommand:theCommandTwo', theCommandTwo)
+  consola.warn('getCommand:isValidCommand(theCommandTwo)', isValidCommand(theCommandTwo))
 
   if(isValidCommand(theCommandOne)) return paramCase? changeCase.paramCase(theCommandOne) : theCommandOne
   if(isValidCommand(theCommandTwo)) return paramCase? changeCase.paramCase(theCommandTwo) : theCommandTwo
