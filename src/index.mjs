32a76376ae171371c2655bbf967c3a4430eba7c6
Randy Houlahan <randy.houlahan@cbd.int>
2022-08-30 10:06:52 -0400

    cleaning

--- src/index.mjs
+++ src/index.mjs
@@ -1,61 +1,62 @@
-const locale  = 'en';
-const locales = [ 'ar', 'en', 'es', 'fr', 'ru', 'zh' ];
+const locale      =   'en'
+const locales     = [ 'ar', 'en', 'es', 'fr', 'ru', 'zh' ]
+const environment =   'test'
 
 const sites =  {
-  acb : { locale, locales: [ 'en', 'zh', 'ms', 'my', 'km', 'lo', 'fil', 'th', 'vi' ], urls: ['www.asean.chm-cbd.net'], redirectTo: 'asean.chm-cbd.net', logo: 'ACB_ASEAN_Logo.png'  },
-  be  : { locale, locales: [ 'en', 'fr', 'nl', 'de'], urls: ['biodiv.be'], redirectTo: 'www.biodiv.be' },
-  bf  : { locale: 'fr', locales:[ 'en', 'fr' ], environment: 'test' },
-  bi  : { locale, locales: [ 'en', 'fr' ] }, 
-  bj  : { locale: 'fr', locales:[ 'en', 'fr' ], environment: 'test' },
-  bt  : { locale, locales: [ 'dz' ] },
-  cd  : { locale: 'fr', locales:[ 'en', 'fr' ], environment: 'test' },
-  cf  : { locale: 'fr', locales:[ 'en', 'fr' ] },
-  cg  : { locale: 'fr', locales:[ 'en', 'fr' ] },  
-  ch  : { locale: 'en', locales:[ 'en', 'fr', 'de', 'it' ], environment: 'test'},
-  ci  : { locale: 'fr', locales:[ 'en', 'fr' ]  },
-  cm  : { locale: 'fr', locales:[ 'en', 'fr' ], environment: 'test' },
-  eg  : { locale: 'ar', locales:[ 'ar', 'en' ], environment: 'test' },
-  et  : { locale, locales:[ 'am', 'en' ] },
-  fr  : { locale: 'fr', locales: [ 'en', 'fr' ], redirectTo:'biodiv.mnhn.fr' },
-  ga  : { locale, locales:[ 'en', 'fr' ] },
-  gh  : { locale, locales:[ 'en', 'fr'], environment: 'test' },
-  gn  : { locale: 'fr', locales:[ 'en', 'fr' ], environment: 'test'  },
-  gw  : { locale: 'pt', locales:[ 'en', 'fr', 'pt' ], environment: 'test' }, 
-  gy  : { locale, locales:['en'], environment: 'test' },
-  id  : { locale, locales: [ 'en', 'fr', 'id' ], environment: 'test' },
-  iq  : { locale: 'ar', locales:[ 'ar', 'en' ] },
-  jo  : { locale, locales:[ 'ar', 'en' ], environment: 'test' },
-  ke  : { locale, locales:[ 'en' ], environment: 'test' },
-  km  : { locale, locales:[ 'en', 'fr' ] },
-  kw  : { locale: 'ar', locales:['ar', 'en'], environment: 'test'},
-  lb  : { locale: 'ar', locales:['ar', 'en'], environment: 'test' },
-  lk  : { locale, locales, environment: 'test' },
-  ma  : { locale, locales: ['ar', 'en', 'fr', 'es'], environment: 'test' },
-  mg  : { locale, locales:[ 'en', 'fr' ], environment: 'test' },
-  ml  : { locale, locales:[ 'en', 'fr' ] },
-  mr  : { locale, locales:[ 'ar','en', 'fr' ] },
-  mu  : { locale, locales:[ 'en', 'fr' ] },
-  my  : { locale, locales:[ 'en', 'fr', 'zh', 'ms'], environment: 'test' },
-  ne  : { locale, locales: ['en', 'fr'], environment: 'test' },
-  nl  : { locale:'nl', locales: ['en', 'nl'] },
-  om  : { locale: 'ar', locales: ['en', 'ar'], environment: 'test' },
-  ph  : { locale, locales:['es', 'en', 'fr', 'fil'], environment: 'test' },
-  ps  : { locale: 'ar', locales: ['en', 'ar'], environment: 'test' },
-  rw  : { locale: 'fr', locales:[ 'en', 'fr' ] },
-  sa  : { locale: 'ar', locales: ['en', 'ar'], environment: 'test' },
-  sd  : { locale: 'ar', locales: ['en', 'ar'] },
-  sg  : { locale, locales: ['en', 'fr', 'zh', 'ms', 'ta'], environment: 'test' },
-  sy  : { locale: 'ar', locales: ['en', 'ar'], environment: 'test' },
-  td  : { locale: 'fr', locales: ['en', 'fr', 'ar'], environment: 'test' },
-  tg  : { locale, locales:[ 'en', 'fr' ], environment: 'test' },
-  th  : { locale, locales: ['en', 'fr', 'zh', 'th'], environment: 'test' },
-  tz  : { locale, locales: ['en', 'sw'] },
-  ug  : { locale, locales: [ 'en' ], environment: 'test' },
-  vn  : { locale: 'vi', locales: [ 'en', 'vi' ], environment: 'test' },
-  www : { locale, locales },
-  ye  : { locale, locales: ['en', 'ar'] },
-  zm  : { locale, locales: [ 'en' ] },
-  zw  : { locale, locales: [ 'en' ] },
+  acb : { locale,       locales: [ 'en', 'zh', 'ms', 'my', 'km', 'lo', 'fil', 'th', 'vi' ], urls: ['www.asean.chm-cbd.net'], redirectTo: 'asean.chm-cbd.net', logo: 'ACB_ASEAN_Logo.png'  },
+  be  : { locale,       locales: [ 'en', 'fr', 'nl', 'de'], urls: ['biodiv.be'], redirectTo: 'www.biodiv.be' },
+  bf  : { locale: 'fr', locales: [ 'en', 'fr' ],             environment },
+  bi  : { locale,       locales: [ 'en', 'fr' ] }, 
+  bj  : { locale: 'fr', locales: [ 'en', 'fr' ],             environment },
+  bt  : { locale,       locales: [ 'dz' ] },
+  cd  : { locale: 'fr', locales: [ 'en', 'fr' ],             environment },
+  cf  : { locale: 'fr', locales: [ 'en', 'fr' ] },
+  cg  : { locale: 'fr', locales: [ 'en', 'fr' ] },  
+  ch  : { locale: 'en', locales: [ 'en', 'fr', 'de', 'it' ], environment},
+  ci  : { locale: 'fr', locales: [ 'en', 'fr' ]  },
+  cm  : { locale: 'fr', locales: [ 'en', 'fr' ],             environment },
+  eg  : { locale: 'ar', locales: [ 'ar', 'en' ],             environment },
+  et  : { locale,       locales: [ 'am', 'en' ] },
+  fr  : { locale: 'fr', locales: [ 'en', 'fr' ], redirectTo: 'biodiv.mnhn.fr' },
+  ga  : { locale,       locales: [ 'en', 'fr' ] },
+  gh  : { locale,       locales: [ 'en', 'fr'],         environment },
+  gn  : { locale: 'fr', locales: [ 'en', 'fr' ],        environment  },
+  gw  : { locale: 'pt', locales: [ 'en', 'fr', 'pt' ],  environment }, 
+  gy  : { locale,       locales: [ 'en'],               environment },
+  id  : { locale,       locales: [ 'en', 'fr', 'id' ],  environment },
+  iq  : { locale: 'ar', locales: [ 'ar', 'en' ] },
+  jo  : { locale,       locales: [ 'ar', 'en' ], environment },
+  ke  : { locale,       locales: [ 'en' ], environment },
+  km  : { locale,       locales: [ 'en', 'fr' ] },
+  kw  : { locale: 'ar', locales: [ 'ar', 'en'], environment},
+  lb  : { locale: 'ar', locales: [ 'ar', 'en'], environment },
+  lk  : { locale,       locales, environment },
+  ma  : { locale,       locales: [ 'ar', 'en', 'fr', 'es'], environment },
+  mg  : { locale,       locales: [ 'en', 'fr' ], environment },
+  ml  : { locale,       locales: [ 'en', 'fr' ] },
+  mr  : { locale,       locales: [ 'ar','en', 'fr' ] },
+  mu  : { locale,       locales: [ 'en', 'fr' ] },
+  my  : { locale,       locales: [ 'en', 'fr', 'zh', 'ms'], environment },
+  ne  : { locale,       locales: [ 'en', 'fr'], environment },
+  nl  : { locale: 'nl', locales: [ 'en', 'nl'] },
+  om  : { locale: 'ar', locales: [ 'en', 'ar'], environment },
+  ph  : { locale,       locales: [ 'es', 'en', 'fr', 'fil'], environment },
+  ps  : { locale: 'ar', locales: [ 'en', 'ar'], environment },
+  rw  : { locale: 'fr', locales: [ 'en', 'fr' ] },
+  sa  : { locale: 'ar', locales: [ 'en', 'ar'], environment },
+  sd  : { locale: 'ar', locales: [ 'en', 'ar'] },
+  sg  : { locale,       locales: [ 'en', 'fr', 'zh', 'ms', 'ta'], environment },
+  sy  : { locale: 'ar', locales: [ 'en', 'ar'], environment },
+  td  : { locale: 'fr', locales: [ 'en', 'fr', 'ar'], environment },
+  tg  : { locale,       locales: [ 'en', 'fr' ], environment },
+  th  : { locale,       locales: [ 'en', 'fr', 'zh', 'th'], environment },
+  tz  : { locale,       locales: [ 'en', 'sw'] },
+  ug  : { locale,       locales: [ 'en' ], environment },
+  vn  : { locale: 'vi', locales: [ 'en', 'vi' ], environment },
+  www : { locale,       locales },
+  ye  : { locale,       locales: [ 'en', 'ar'] },
+  zm  : { locale,       locales: [ 'en' ] },
+  zw  : { locale,       locales: [ 'en' ] },
 
   'han-demo'  : { locale, locales },
   demo        : { locale, locales },
@@ -71,9 +72,24 @@ const c = (() =>{
     const routePriority =  2
     const siteCodes     = Object.keys(sites)
 
+    addHostToSites(sites)
+
     return { name, sites, siteCodes, routePriority, httpPort, httpsPort, hostDbPort }
 })()
 
 export const config = c
 
 export default c
+
+function getHost(siteCode, {redirectTo, environment}){
+  if(redirectTo) return redirectTo
+
+  if(environment) return `${siteCode}.test.chm-cbd.net`
+
+  return `${siteCode}.chm-cbd.net`
+}
+
+function addHostToSites(sites){
+  for (let sideCode of Object.keys(sites))
+    sites[sideCode].host = getHost(siteCode, sites[sideCode])
+}
